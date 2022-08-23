60d8150399a4265a8baeb6599bebf2811434f364
jowel007 <rexjowel@gmail.com>
2022-08-23 16:54:08 +0600

    Category module-I

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,8 @@ class DatabaseSeeder extends Seeder
         //$this->call(VendorsTableSeeder::class);
         //$this->call(VendorsBusinessDetailsTableSeeder::class);
         //$this->call(VendorsBankDetailsTableSeeder::class);
-        $this->call(SectionTableSeeder::class);
+        // $this->call(SectionTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
 
     }
 }
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -65,7 +65,7 @@
             </a>
             <div class="collapse" id="ui-catelogue">
               <ul class="nav flex-column sub-menu" style="background :#fff !important; color:#4B49AC !important">
-                <li class="nav-item"> <a @if(Session::get("page")=="sctions") style="background: #4B49AC !important; color:#fff !important" @else style="background: #fff !important; color:#4B49AC !important"  @endif class="nav-link" href="{{ url('admin/sections') }}">Sections</a></li>
+                <li class="nav-item"> <a @if(Session::get("page")=="sctions") style="background: #4B49AC !important; color:#fff !important" @else style="background: #4B49AC !important; color:#fff !important"  @endif class="nav-link" href="{{ url('admin/sections') }}">Sections</a></li>
                 <li class="nav-item"> <a @if(Session::get("page")=="categories") style="background: #4B49AC !important; color:#fff !important" @else style="background: #fff !important; color:#4B49AC !important"  @endif class="nav-link" href="{{ url('admin/categories') }}">Categories</a></li>
                 <li class="nav-item"> <a @if(Session::get("page")=="products") style="background: #4B49AC !important; color:#fff !important" @else style="background: #fff !important; color:#4B49AC !important"  @endif class="nav-link" href="{{ url('admin/products') }}">Products</a></li>
               </ul>
